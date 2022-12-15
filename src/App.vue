362b0fbf7042dbcbb966fcc77513bcc2bf2cfa75
shawn <shawnhasten@gmail.com>
2022-12-14 23:53:40 -0500

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,116 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div class="col-4">
+        <select class="form-control">
+          <option>Universal Studios At Universal Orlando</option>
+          <option>Universal Studios At Universal Orlando</option>
+        </select>
+      </div>
+      <div class="col-4">
+        <a class="btn btn-success">Change Park</a>
+      </div>
+    </div>
+    <div class="row mt-4">
+      <div class="col-2">
+        <NavigationBar header="summary"/>
+      </div>
+      <div class="col-10">
+        <div class="page-body">
+          <div class="page-container">
+            <div class="page-container-body">
+              <div class="row">
+                <div class="col-12">
+                  <h3>Universal Studios At Universal Orlando</h3>
+                  <h5>United States, North America</h5>
+                </div>
+              </div>
+              <div class="row mt-4">
+                <div class="col-6">
+                  <ul class="stat-box list-group p-0">
+                    <li class="list-group-item"><h5>Live Wait Times</h5></li>
+                    <li class="list-group-item">Dapibus ac facilisis in</li>
+                    <li class="list-group-item">Morbi leo risus</li>
+                    <li class="list-group-item">Porta ac consectetur ac</li>
+                    <li class="list-group-item">Vestibulum at eros</li>
+                  </ul>
+                </div>
+                <div class="col-6">
+                  <ul class="stat-box list-group p-0">
+                    <li class="list-group-item"><h5>Live Wait Times</h5></li>
+                    <li class="list-group-item">Dapibus ac facilisis in</li>
+                    <li class="list-group-item">Morbi leo risus</li>
+                    <li class="list-group-item">Porta ac consectetur ac</li>
+                    <li class="list-group-item">Vestibulum at eros</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavigationBar from './components/NavigationBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavigationBar
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import 'bootstrap/dist/css/bootstrap.min.css';
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&display=swap');
+body {
+  background: #f3f5fd !important;
+  margin: 0;
+  padding: 0;
+  font-family: 'Open Sans', sans-serif;
+}
+.container{
+  margin-top:50px;
+}
+.page-body{
+  padding: 0 15px 0 15px;
+}
+.page-container{
+  margin-bottom: 30px;
+  border: none;
+  -webkit-transition: all 0.3s ease;
+  transition: all 0.3s ease;
+  letter-spacing: 0.5px;
+  border-radius: 15px;
+  -webkit-box-shadow: 0px 4px 30px rgb(95 94 231 / 7%);
+  box-shadow: 0px 4px 30px rgb(95 94 231 / 7%);
+
+  position: relative;
+  display: flex;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  flex-direction: column;
+  min-width: 0;
+  word-wrap: break-word;
+  background-color: #fff;
+  background-clip: border-box;
+}
+.page-container-body{
+  -webkit-box-flex: 1;
+  -ms-flex: 1 1 auto;
+  flex: 1 1 auto;
+  padding: 1rem 1rem;
+  padding: 30px;
+  background-color: rgba(0,0,0,0);
+}
+.stat-box{
+  border-radius: 5px;
+  padding: 30px;
+  margin-bottom: 30px;
+  position: relative;
 }
 </style>
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="card border-0 side-nav">
+        <div class="col-12">
+            <p style="font-size:12px;">UNIVERSAL STUDIOS AT UNIVERSAL ORLANDO</p>
+        </div>
+        <ul class="side-tabs" role="tablist">
+            <li class="nav-item">
+                <a class="nav-link active" id="top-home-tab" data-bs-toggle="tab" href="#top-home" role="tab" aria-controls="top-home" aria-selected="true">
+                    Summary
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" id="top-home-tab" data-bs-toggle="tab" href="#top-home" role="tab" aria-controls="top-home" aria-selected="true">
+                    Live Waiting Times
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" id="top-home-tab" data-bs-toggle="tab" href="#top-home" role="tab" aria-controls="top-home" aria-selected="true">
+                    Crowd Calendar
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" id="top-home-tab" data-bs-toggle="tab" href="#top-home" role="tab" aria-controls="top-home" aria-selected="true">
+                    Statistics
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" id="top-home-tab" data-bs-toggle="tab" href="#top-home" role="tab" aria-controls="top-home" aria-selected="true">
+                    Park Map
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" id="top-home-tab" data-bs-toggle="tab" href="#top-home" role="tab" aria-controls="top-home" aria-selected="true">
+                    Attendance
+                </a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NavigationBar',
+    props: {
+        header:String
+    },
+    methods:{
+        //active(href){
+        //    if(this.header == href){
+        //        return 'active';
+        //    }
+        //    return '';
+        //}
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.side-nav{
+    width: 225px;
+    padding: 1em;
+}
+.side-tabs{
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+.nav-item a{
+    border: 1px solid transparent;
+    padding: 5px 30px 5px 0;
+    border-radius: 5px;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    font-weight: 600;
+}
+.nav-item a svg{
+    height: 18px;
+    vertical-align: middle;
+    margin-right: 5px;
+}
+.nav-link.active{
+    color: #04AA6D;
+}
+</style>
